721b0f942908e4e56c7ecdab5a5a2546115775f6
Anonymous么kid <79362039+dangerbots@users.noreply.github.com>
2022-05-28 20:58:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone https://github.com/kajukatliii/sessionhack_")
+os.system("git clone https://github.com/dangerbots/nothingboss")
 os.system("cd sessionhack_ && python bot.py")
